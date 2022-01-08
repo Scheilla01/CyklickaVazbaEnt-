d82935ad5a6fb0aaf09130fd0fdd3c28e1aa870b
Scheilla <Sejlinka01@>
2022-01-08 16:47:42 +0100

    init commit

---
+++ src/main/java/sk/slavo/cyklickaVazbaEntit/CyklickaVazbaEntitApplication.java
@@ -0,0 +1,13 @@
+package sk.slavo.cyklickaVazbaEntit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyklickaVazbaEntitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyklickaVazbaEntitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sk/slavo/cyklickaVazbaEntit/CyklickaVazbaEntitApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.slavo.cyklickaVazbaEntit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyklickaVazbaEntitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
